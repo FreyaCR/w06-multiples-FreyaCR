5f1b1b430c8442b0e5c703d5ad111769f8cb7a3b
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-09-13 23:23:30 +1000

    Created main and result activity layouts and added click listener in main to launch result activity using intent

--- app/src/main/java/au/edu/swin/sdmd/w06_multiples/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w06_multiples/MainActivity.kt
@@ -1,11 +1,19 @@
 package au.edu.swin.sdmd.w06_multiples
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout = findViewById<ConstraintLayout>(R.id.layout)
+        layout.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
