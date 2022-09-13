aee763398f4b24c1a6543f79408358791dc98062
Freya-The-Goddess <106995979+Freya-The-Goddess@users.noreply.github.com>
2022-09-14 00:13:16 +1000

    Added functionality to randomly generate factors and pass result to result activity using intent extra

--- app/src/main/java/au/edu/swin/sdmd/w06_multiples/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w06_multiples/MainActivity.kt
@@ -3,17 +3,33 @@ package au.edu.swin.sdmd.w06_multiples
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val layout = findViewById<ConstraintLayout>(R.id.layout)
-        layout.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
+        //randomly generate two integer factors 1-12
+        val factor1 = Random.nextInt(from = 1, until = 13)
+        val factor2 = Random.nextInt(from = 1, until = 13)
+        val result = factor1 * factor2
+
+        //populate factor text views with generated values
+        val factor1View = findViewById<TextView>(R.id.factor1)
+        val factor2View = findViewById<TextView>(R.id.factor2)
+        factor1View.text = factor1.toString()
+        factor2View.text = factor2.toString()
+
+        //layout click listener to start result activity
+        val layoutView = findViewById<ConstraintLayout>(R.id.layout)
+        layoutView.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("result", result) //int extra
+            }
+            startActivity(intent) //start result activity
         }
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/w06_multiples/ResultActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w06_multiples/ResultActivity.kt
@@ -2,10 +2,19 @@ package au.edu.swin.sdmd.w06_multiples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        //get result from intent extra
+        val result = intent.getIntExtra("result",0)
+
+        //populate result
+        val resultView = findViewById<TextView>(R.id.result)
+        resultView.text = result.toString()
+
     }
 }
